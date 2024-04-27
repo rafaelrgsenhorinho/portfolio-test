0eb47f8b3d58feb605922f9c42dbb638641bcb78
Rafael <rafaelrgsenhorinho@gmail.com>
2024-04-27 14:35:35 -0300

    project complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,38 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <h3 ${project.github ? 'class="portfolio-title github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('')
+    
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return `
+        <li>
+            <h3 class="exprerience-title">${experience.name}</h3>
+            <p class="period">${experience.period}</p>
+            <p>
+                ${experience.description}    
+            </p>
+        </li>
+        `
+    }).join('')
+}
 
 (async () => {
 
@@ -40,6 +72,9 @@ function updateHardSkills(profileData) {
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
 
 
 
